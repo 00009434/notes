835be6dc66d9727673ad4f3b333627bcadb1ed4e
Ulugbek Otajonov <00009434@wiut.uz>
2021-03-05 17:47:10 +0500

    add styles

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use('/static', express.static('public'))
+
 app.set('view engine', 'pug')
 
 
 app.get('/', (req, res) => {
+	res.render('home')
 
 })
 
