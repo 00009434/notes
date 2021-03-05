d537150bc8cfcbfda5f88a5374fa870ad7b2733c
Ulugbek Otajonov <00009434@wiut.uz>
2021-03-05 16:54:24 +0500

    start from

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'pug')
+
+
+app.get('/', (req, res) => {
+
+})
+
+app.listen(8000, err => {
+	if (err) throw err
+
+	console.log('App is running...')
+})
